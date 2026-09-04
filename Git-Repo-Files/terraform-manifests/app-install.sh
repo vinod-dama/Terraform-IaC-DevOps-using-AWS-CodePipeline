f299bb291a13ec0c05e903b3dcf9dfaffdebb048
Vinod Dama <vinod.dama@gmail.com>
2026-09-04 07:52:10 +0530

    terraform destroy

---
+++ Git-Repo-Files/terraform-manifests/app-install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+sudo dnf update -y
+sudo dnf install -y httpd
+sudo systemctl enable httpd
+sudo service httpd start  
+sudo echo '<h1>Welcome to Vinods Project - Build Terrform Code on AWS using CodePipeline on Multi env </h1>' | sudo tee /var/www/html/index.html
+sudo mkdir /var/www/html/app1
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(260, 220, 220);"> <h1>Welcome to Building AWS infra across multi env via CodePipeline </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+TOKEN=$(curl -s -S -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+sudo curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+
+
+
