93918c8d11232b28cc441cd46ff92ac3f54e1b05
Vinod Dama <vinod.dama@gmail.com>
2026-09-05 07:55:35 +0530

    app update

--- Git-Repo-Files/terraform-manifests/app-install.sh
+++ Git-Repo-Files/terraform-manifests/app-install.sh
@@ -164,7 +164,7 @@ sudo tee /var/www/html/index.html > /dev/null <<'EOF'
         </div>
 
         <div class="cta-buttons">
-            <a href="https://www.youtube.com/@VinoSVoyage" target="_blank" class="btn btn-primary">✈️ Subscribe to Vino's Voyage</a>
+            <a href="https://www.youtube.com/@VinoSVoyage/featured" target="_blank" class="btn btn-primary">✈️ Subscribe to Vino's Voyage</a>
             <a href="/app1/metadata.html" class="btn btn-secondary">🔍 View EC2 Metadata (IMDSv2)</a>
         </div>
     </div>
